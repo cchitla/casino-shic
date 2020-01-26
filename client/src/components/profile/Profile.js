5e90fa9df9fcb39f429ea6875a4b66675e866d4d
Chris Chitla <chrischitla@gmail.com>
2020-01-26 14:40:43 -0700

    added basic auth setup

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { useAuth0 } from '../auth/auth0/Auth0';
 
 const Profile = (props) => {
+  const { loading, user } = useAuth0();
+
+  if (loading || !user) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Container className="pt-3">
       <Row className="text-center">
         <Col className="m-2 p-3">
           <h1>This is Profile page!</h1>
+
+          <img src={user.picture} alt="Profile" />
+
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+          <code>{JSON.stringify(user, null, 2)}</code>
         </Col>
       </Row>
     </Container>
