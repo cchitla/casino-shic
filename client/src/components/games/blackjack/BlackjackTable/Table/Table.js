7df78c3467c1e094242494bf2ab8679abb289b95
Chris Chitla <chrischitla@gmail.com>
2020-02-18 20:34:55 -0700

    styling for blackjack table

--- client/src/components/games/blackjack/BlackjackTable/Table/Table.js
+++ client/src/components/games/blackjack/BlackjackTable/Table/Table.js
@@ -15,19 +15,19 @@ const Table = (props) => {
 
   const renderBetButtons = () => {
     return (
-      <span id="blackjackButtons">
-        <button value={5} onClick={e => placeBet(e)}>Bet 5</button>
-        <button value={10} onClick={e => placeBet(e)}>Bet 10</button>
-      </span>
+      <div id="blackjackButtons">
+        <button className="custom-btn" value={5} onClick={e => placeBet(e)}>Bet 5</button>
+        <button className="custom-btn" value={10} onClick={e => placeBet(e)}>Bet 10</button>
+      </div>
     );
   };
 
   const renderGameButtons = () => {
     return (
-      <span id="blackjackButtons">
-        <button onClick={props.hitCard}>Hit</button>
-        <button onClick={props.stayHand} >Stay</button>
-      </span>
+      <div id="blackjackButtons">
+        <button className="custom-btn" onClick={props.hitCard}>Hit</button>
+        <button className="custom-btn" onClick={props.stayHand} >Stay</button>
+      </div>
     );
   };
 
@@ -38,27 +38,26 @@ const Table = (props) => {
   };
 
   const renderButtons = () => {
-    if (!props.gameIsActive) return <button onClick={startGame}>Start Game</button>;
+    if (!props.gameIsActive) return <button className="custom-btn" onClick={startGame}>Start Game</button>;
     if (props.betting && !props.playerBet) return renderBetButtons();
     if (props.currentTurn && !props.winners) return renderGameButtons();
   };
 
   const renderWinners = (winners) => {
-    if (winners) {
-      return <p>Winner: {winners.name}</p>
-    };
+    if (winners) return <p>Winner: {winners.name}</p>
     return <p>No one won this hand</p>
   };
 
   return (
     <>
     <div id="blackjackTable">
-    <Row>
+    <Row id="game-row">
       <Col>
         <div id="dealerInfo">
           <div>Dealer</div>
-          <div>Dealer Score: {props.dealer ? props.dealer[0].score : ""}</div>
-          <div>Dealer Hand<div>
+          {console.log(props.dealer)}
+          <div>Dealer Score: {props.dealer && props.dealer[0].currentTurn ? props.dealer[0].score : ""}</div>
+          <div>Dealer hand<div>
               {props.dealer
                 ?  props.dealer[0].hand.map((card) => (
                   <Card key={Math.random()} suit={card.suit} cardName={card.cardName} />)) : null}
@@ -71,21 +70,25 @@ const Table = (props) => {
         <div id="blackjackPlayerInfo">
           <div>{props.joinedPlayers.map((player) => <span key={player.name}>{player.name}</span>)}</div> 
           <div>Your score: {props.playerScore}</div>
-          <div> {props.playerBust && <span>You lose!</span>} </div>
-          <div>{props.playerHand && <span>Your hand:</span>}</div>
+          <div>{props.playerHand && <span>Your hand</span>}</div>
         </div>
 
         <div>{props.playerHand
           ? props.playerHand.map((hand) => (
             <Card key={Math.random()} suit={hand.suit} cardName={hand.cardName} />)) : null}
         </div>
-        <div>{props.winners ? renderWinners(props.winners) : null}</div>
         </Col>
-      
-    
+        
     </Row>
+    <Row>
+      <Col>
+      <div className="winner-text text-center">{props.winners ? renderWinners(props.winners) : null}</div>
+      </Col>
+    </Row>
+  
     </div>
-    {renderButtons()}
+    <div className="game-buttons text-center">{renderButtons()}</div>
+   
     </>
   );
 };
